1e6a5e928cdb4c74583c4770429eb11114714921
Reynaldo <jamjrs@gmail.com>
2012-06-22 17:25:02 -0300

    before implementation of new slide on home

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php if(get_option('13floor_featured') == 'on') { ?>
 	<?php get_header(); ?>
 
-	<?php get_template_part('includes/featured'); ?>
+	<?php get_template_part('includes/featured-tec'); ?>
 
 	<?php get_footer(); ?>
 <?php } else { ?>
